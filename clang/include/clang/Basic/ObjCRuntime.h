14d0edf7a8b06ea65d8f7812ef6792a3ef0215b6
hmelder <service@hugomelder.com>
2024-01-01 23:00:20 +0100

    Add new fast-path platforms in ObjcRuntime.h

--- clang/include/clang/Basic/ObjCRuntime.h
+++ clang/include/clang/Basic/ObjCRuntime.h
@@ -100,11 +100,18 @@ public:
   bool isLegacyDispatchDefaultForArch(llvm::Triple::ArchType Arch) {
     // The GNUstep runtime uses a newer dispatch method by default from
     // version 1.6 onwards
-    if (getKind() == GNUstep && getVersion() >= VersionTuple(1, 6)) {
-      if (Arch == llvm::Triple::arm ||
-          Arch == llvm::Triple::x86 ||
-          Arch == llvm::Triple::x86_64)
-        return false;
+    if (getKind() == GNUstep) {
+      switch (Arch) {
+        case llvm::Triple::arm:
+        case llvm::Triple:x86:
+        case llvm::Triple:x86_64:
+          return !(getVersion() >= VersionTuple(1, 6));
+        case llvm::Triple::aarch64:
+        case llvm::Triple::mips64:
+          return !(getVersion() >= VersionTuple(1, 9));
+        case llvm::Triple::riscv64:
+          return !(getVersion() >= VersionTuple(2, 2));
+      }
     }
     else if ((getKind() ==  MacOSX) && isNonFragile() &&
              (getVersion() >= VersionTuple(10, 0)) &&
